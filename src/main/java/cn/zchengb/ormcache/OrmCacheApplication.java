86c2a660cc62fb7a768fbbf3e9ce9867e7fbaa54
xiaobin <xiaobin.zheng@thoughtworks.com>
2024-07-27 16:28:02 +0800

    feat: initialize project structure.

---
+++ src/main/java/cn/zchengb/ormcache/OrmCacheApplication.java
@@ -0,0 +1,13 @@
+package cn.zchengb.ormcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/zchengb/ormcache/OrmCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zchengb.ormcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
